f3f82b88b7459afb59f485a46757936f0f1b078a
aaa <it-z@tut.by>
2018-03-05 18:27:21 +0300

    it just seems my cpu is not fast enough

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+	
+	let zeros = 0;
+	
+	function numberOfTwos (num) {
+		
+		let result = 0;
+		
+		while (num % 2 == 0) {
+			num /= 2;
+			result += 1
+		}
+		
+		return result;
+	
+	}
+  
+	function numberOfFives (num) {
+		
+		let result = 0;
+		
+		while (num % 5 == 0) {
+			num /= 5;
+			result += 1
+		}
+		
+		return result;
+	
+	}
+	
+	let overallNumberOfTwos = 0;
+	let overallNumberOfFives = 0;
+	
+	for (i = 2; i <= number; i++)	{
+	
+		overallNumberOfTwos += numberOfTwos (i);
+		overallNumberOfFives += numberOfFives (i);
+	
+	}
+	
+	if (overallNumberOfTwos !=0 && overallNumberOfFives != 0)	{
+		
+		if (overallNumberOfFives > overallNumberOfTwos)	{
+			
+			zeros = overallNumberOfTwos;
+	
+		}	else {
+				zeros = overallNumberOfFives;
+			}
+		
+	}
+		
+	return zeros;	
 }
